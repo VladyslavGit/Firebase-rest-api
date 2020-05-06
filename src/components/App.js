d0947d953e54cf0516caa6e96707b1baf6f57c22
Vladyslav <vladyslav.work@gmail.com>
2020-05-06 21:43:26 +0300

    Add form for sending data

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Form } from "./form/Form";
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div className={styles.wrapper}>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/form/Form.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import styles from "./Form.module.css";
+
+export class Form extends Component {
+  state = {};
+  render() {
+    return (
+      <>
+        <h2 className={styles.title}>Manage your academic plan</h2>
+        <form className={styles.form}>
+          <label htmlFor="language" className={styles.select}>
+            Select a programming language:
+            <select
+              className={styles.selectList}
+              value={this.state.language}
+              onChange={this.onChangeSelect}
+            >
+              <option value="C++">C++</option>
+              <option value="Java">Java</option>
+              <option value="C#">C#</option>
+              <option value="JavaScript">JavaScript</option>
+              <option value="Scala">Scala</option>
+            </select>
+          </label>
+          <label htmlFor="data" className={styles.labelForDate}>
+            Choose a graduation date:
+            <input
+              className={styles.selectList}
+              type="date"
+              name="searchQuery"
+              onChange={this.handleImputChange}
+              autoComplete="off"
+              onKeyDown={this.find}
+            />
+          </label>
+          <label htmlFor="comments">
+            <input
+              className={styles.input}
+              type="text"
+              name="searchQuery"
+              onChange={this.handleImputChange}
+              id={this.inputId}
+              placeholder="Add your comments ..."
+              autoComplete="off"
+              onKeyDown={this.find}
+            />
+          </label>
+          <button type="submit" className={styles.btnForSelect}>
+            Send
+          </button>
+        </form>
+      </>
+    );
+  }
+}
